08b5da55b9bd23ca91b45b272e2b610c37356e11
dtonavitor <vitor.barros@sou.inteli.edu.br>
2024-02-02 10:22:54 -0300

    feat: payment proccess

---
+++ src/payment/pkg/rabbitmq/rabbitmq.go
@@ -0,0 +1,58 @@
+package rabbitmq
+
+import (
+	"context"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func OpenChannel() (*amqp.Channel, error) {
+	conn, error := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if error != nil {
+		panic(error)
+	}
+
+	ch, error := conn.Channel()
+	if error != nil {
+		panic(error)
+	}
+	return ch, nil
+}
+
+func Consume(ch *amqp.Channel, out chan amqp.Delivery, queue string) error {
+	msgs, err := ch.Consume(
+		queue,
+		"go-payment",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	for msg := range msgs {
+		out <- msg
+	}
+	return nil
+}
+
+func Publish(ctx context.Context, ch *amqp.Channel, body, exName string) error {
+	err := ch.PublishWithContext(
+		ctx,
+		exName,
+		"",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/json",
+			Body:        []byte(body),
+		},
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
